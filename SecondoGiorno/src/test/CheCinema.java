ad45dea7273186a033070be180abcf31f281e956
PieroVaghi <piero.vaghi@gmail.com>
2019-10-02 17:50:09 +0200

    Add files via upload

--- SecondoGiorno/src/test/CheCinema.java
+++ SecondoGiorno/src/test/CheCinema.java
@@ -3,20 +3,27 @@ package test;
 public class CheCinema {
 
 	public static void main(String[] args) {
-
-		boolean isGenereM = false;
-		boolean isTifoso = true;
-		String giorno = "martedi";
+		
+			//VARIABILI SU UTENTE
+		String genere = "m";
+		String tifoso = "s";
+		String giorno = "martedì";
+			//VARIABILI CONDIZIONI
+		boolean isLun = giorno.equalsIgnoreCase("lunedì");
+		boolean isMar = giorno.equalsIgnoreCase("martedì");
+		boolean isDom = giorno.equalsIgnoreCase("domenica");
+		boolean isM = genere.equalsIgnoreCase("m");
+		boolean isTifoso = tifoso.equalsIgnoreCase("s");
 		
 		double prezzo = 9.50;
 		
-		if((giorno.equalsIgnoreCase("lunedì") && !isGenereM)||(giorno.equalsIgnoreCase("martedì") && isTifoso))
+		if((isLun && !isM)||(isMar && isTifoso))
 			prezzo = 4.50;
 		
 //		if(giorno.equalsIgnoreCase("martedì") && isTifoso)
 //			prezzo = 4.50;
 		
-		if(giorno.equalsIgnoreCase("domenica"))
+		if(isDom)
 			prezzo = 2.90;
 		
 		String risposta = "Il biglietto costa: " + prezzo + "€";
