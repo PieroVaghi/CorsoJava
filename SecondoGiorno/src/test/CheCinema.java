f85e49f865fd00b0331e9570f66d0f48c4218567
PieroVaghi <piero.vaghi@gmail.com>
2019-10-02 17:38:35 +0200

    Add files via upload

--- SecondoGiorno/src/test/CheCinema.java
+++ SecondoGiorno/src/test/CheCinema.java
@@ -3,16 +3,19 @@ package test;
 public class CheCinema {
 
 	public static void main(String[] args) {
-		boolean isGenereM = true;
+
+		boolean isGenereM = false;
 		boolean isTifoso = true;
+		String giorno = "martedi";
+		
 		double prezzo = 9.50;
-		String giorno = "domenica";
 		
+		if((giorno.equalsIgnoreCase("lunedì") && !isGenereM)||(giorno.equalsIgnoreCase("martedì") && isTifoso))
+			prezzo = 4.50;
 		
-		if(giorno.equalsIgnoreCase("lunedì") && !isGenereM)
-			prezzo = 4.50;
-		if(giorno.equalsIgnoreCase("martedì") && isTifoso)
-			prezzo = 4.50;
+//		if(giorno.equalsIgnoreCase("martedì") && isTifoso)
+//			prezzo = 4.50;
+		
 		if(giorno.equalsIgnoreCase("domenica"))
 			prezzo = 2.90;
 		
