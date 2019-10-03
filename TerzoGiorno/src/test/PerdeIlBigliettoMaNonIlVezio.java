33224bcb3ea46ecc1911b2297082881e52ff7933
PieroVaghi <piero.vaghi@gmail.com>
2019-10-03 12:33:08 +0200

    Fatto da me - prima consegna
    Esercizio sui biglietti di Vezio

---
+++ TerzoGiorno/src/test/PerdeIlBigliettoMaNonIlVezio.java
@@ -0,0 +1,46 @@
+package test;
+import java.util.Scanner;
+
+public class PerdeIlBigliettoMaNonIlVezio {
+
+	public static void main(String[] args) {
+		Scanner tastiera = new Scanner(System.in);
+		String resi1 = "Varenna";
+		String resi2 = "Vezio";
+		String resi3 = "Perledo";
+		String prof1 = "Studente";
+		String prof2 = "Docente";
+		double biglietto = 10.0;
+		boolean isEtaSconto = false;
+		boolean isResidente = false;
+		boolean isProfSconto = false;
+		
+		System.out.println("Buongiorno!\nGrazie per aver scelto il nostro sistema di trasporto lacustre!");
+		System.out.println("Quanti hanni ha?");
+		int eta = Integer.parseInt(tastiera.nextLine());
+		System.out.println("In che comune risiede?");
+		String residenza = tastiera.nextLine();
+		System.out.println("Qual'è la sua attuale professione?");
+		String professione = tastiera.nextLine();
+		
+		if(eta<5 || eta>70)
+			isEtaSconto = true;
+		if(residenza.equalsIgnoreCase(resi1)||residenza.equalsIgnoreCase(resi2)||residenza.equalsIgnoreCase(resi3))
+			isResidente = true;
+		if(professione.equalsIgnoreCase(prof1)||professione.equalsIgnoreCase(prof2))
+			isProfSconto = true;
+		
+		if(isEtaSconto)
+			biglietto -= 5;
+		if(isProfSconto)
+			biglietto -= 2;
+		if(isResidente)
+			biglietto = 0;
+		
+		String risposta = "Il suo biglietto ha un costo di: " + biglietto + " €!\nBuona Giornata!";
+		System.out.println(risposta);
+		
+		tastiera.close();
+	}
+
+}
