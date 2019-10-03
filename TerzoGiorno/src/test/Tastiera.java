10ca9e0c1b02b8e5a69b6301e4d15fd144521600
PieroVaghi <piero.vaghi@gmail.com>
2019-10-03 12:06:40 +0200

    Versione finale

--- TerzoGiorno/src/test/Tastiera.java
+++ TerzoGiorno/src/test/Tastiera.java
@@ -19,9 +19,28 @@ public class Tastiera {
 		int eta = Integer.parseInt(valoreEta);
 		System.out.println("Di che genere sei? M/F");
 		String genere = tastiera.nextLine().toUpperCase();
-		boolean isGenerM = genere.equals("M");
+		boolean isM = genere.equals("M");
 		System.out.println("Tifoso? S/N");
 		boolean isTifoso = tastiera.nextLine().equalsIgnoreCase("S");
+		boolean isLun = giorno.equalsIgnoreCase("LUN");
+		boolean isMar = giorno.equalsIgnoreCase("MAR");
+		boolean isDom = giorno.equalsIgnoreCase("DOM");
+		
+		double prezzo = 9.50;
+		double sconto1 = 5;
+		double sconto2 = 6.60;
+		
+		if((isLun && isM)||(isMar && isTifoso))
+			prezzo -= sconto1;
+		
+		if(isDom)
+			prezzo -= sconto2;
+		
+		
+		String risposta = "Ciao " + nome + "! Il tuo biglietto costa: " + prezzo + " €";
+		
+		System.out.println(risposta);
+		
 
 	}
 
