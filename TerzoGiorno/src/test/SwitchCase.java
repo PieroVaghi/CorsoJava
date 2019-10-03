043d7f09cf23f3a9b583431718b0d5f0e7ae7a13
PieroVaghi <piero.vaghi@gmail.com>
2019-10-03 15:34:07 +0200

    Diviene uno Switch

--- TerzoGiorno/src/test/SwitchCase.java
+++ TerzoGiorno/src/test/SwitchCase.java
@@ -12,34 +12,78 @@ public class SwitchCase {
 		String mese = tastiera.nextLine();
 		String risposta = "";
 		
-		if(mese.equalsIgnoreCase("gennaio"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("febbraio"))
-			risposta = "28 giorni";
-		if(mese.equalsIgnoreCase("marzo"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("aprile"))
-			risposta = "30 giorni";
-		if(mese.equalsIgnoreCase("maggio"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("giugno"))
-			risposta = "30 giorni";
-		if(mese.equalsIgnoreCase("luglio"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("agosto"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("settembre"))
-			risposta = "30 giorni";
-		if(mese.equalsIgnoreCase("ottobre"))
-			risposta = "31 giorni";
-		if(mese.equalsIgnoreCase("novembre"))
-			risposta = "30 giorni";
-		if(mese.equalsIgnoreCase("dicembre"))
-			risposta = "31 giorni";
-	
+//		if(mese.equalsIgnoreCase("gennaio"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("febbraio"))
+//			risposta = "28/29 giorni";
+//		if(mese.equalsIgnoreCase("marzo"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("aprile"))
+//			risposta = "30 giorni";
+//		if(mese.equalsIgnoreCase("maggio"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("giugno"))
+//			risposta = "30 giorni";
+//		if(mese.equalsIgnoreCase("luglio"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("agosto"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("settembre"))
+//			risposta = "30 giorni";
+//		if(mese.equalsIgnoreCase("ottobre"))
+//			risposta = "31 giorni";
+//		if(mese.equalsIgnoreCase("novembre"))
+//			risposta = "30 giorni";
+//		if(mese.equalsIgnoreCase("dicembre"))
+//			risposta = "31 giorni";
+
+		switch(mese)
+		{
+			case"gennaio":
+				risposta = "31 giorni";
+				break;
+			case"febbraio":
+				risposta = "28/29 giorni";
+				break;
+			case"marzo":
+				risposta = "31 giorni";
+				break;
+			case"aprile":
+				risposta = "30 giorni";
+				break;
+			case"maggio":
+				risposta = "31 giorni";
+				break;
+			case"giugno":
+				risposta = "30 giorni";
+				break;
+			case"luglio":
+				risposta = "31 giorni";
+				break;
+			case"agosto":
+				risposta = "31 giorni";
+				break;
+			case"settembre":
+				risposta = "30 giorni";
+				break;
+			case"ottobre":
+				risposta = "31 giorni";
+				break;
+			case"novembre":
+				risposta = "30 giorni";
+				break;
+			case"dicembre":
+				risposta = "31 giorni";
+				break;
+			default:
+				risposta = "Ma scrivi giusto!!!";
+				break;
+		}
+		
+		
 		System.out.println(risposta);
 		
-		
+		tastiera.close();
 	}
 
 }
