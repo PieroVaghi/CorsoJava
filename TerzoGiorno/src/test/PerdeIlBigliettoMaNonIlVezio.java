a102d293b68105b8d5b36c891298a6f4674e3e34
PieroVaghi <piero.vaghi@gmail.com>
2019-10-03 14:21:47 +0200

    Refactoring 1 - correzione prof

--- TerzoGiorno/src/test/PerdeIlBigliettoMaNonIlVezio.java
+++ TerzoGiorno/src/test/PerdeIlBigliettoMaNonIlVezio.java
@@ -11,29 +11,25 @@ public class PerdeIlBigliettoMaNonIlVezio {
 		String prof1 = "Studente";
 		String prof2 = "Docente";
 		double biglietto = 10.0;
-		boolean isEtaSconto = false;
-		boolean isResidente = false;
-		boolean isProfSconto = false;
-		
+				
 		System.out.println("Buongiorno!\nGrazie per aver scelto il nostro sistema di trasporto lacustre!");
 		
 		System.out.println("In che comune risiede?");
 		String residenza = tastiera.nextLine();
-		if(residenza.equalsIgnoreCase(resi1)||residenza.equalsIgnoreCase(resi2)||residenza.equalsIgnoreCase(resi3))
-			isResidente = true;
+		boolean isResidente = residenza.equalsIgnoreCase(resi1)
+							||residenza.equalsIgnoreCase(resi2)
+							||residenza.equalsIgnoreCase(resi3);
 		
 		if(!isResidente) {
+			
 			System.out.println("Quanti hanni ha?");
 			int eta = Integer.parseInt(tastiera.nextLine());
+			boolean isEtaSconto = eta<5 || eta>70;
+			
 			System.out.println("Qual'è la sua attuale professione?");
 			String professione = tastiera.nextLine();
-			
-			if(eta<5 || eta>70)
-				isEtaSconto = true;
-			if(residenza.equalsIgnoreCase(resi1)||residenza.equalsIgnoreCase(resi2)||residenza.equalsIgnoreCase(resi3))
-				isResidente = true;
-			if(professione.equalsIgnoreCase(prof1)||professione.equalsIgnoreCase(prof2))
-				isProfSconto = true;
+			boolean isProfSconto = professione.equalsIgnoreCase(prof1)
+								||professione.equalsIgnoreCase(prof2);	
 			
 			if(isEtaSconto)
 				biglietto -= 5;
