1dedd7f5382ea570b37d70daac67d1235aa6f6ab
PieroVaghi <piero.vaghi@gmail.com>
2019-10-04 10:52:47 +0200

    Inizio correzione prof con mie idee

--- TerzoGiorno/src/test/Scomputeriamo.java
+++ TerzoGiorno/src/test/Scomputeriamo.java
@@ -7,59 +7,70 @@ public class Scomputeriamo {
 	public static void main(String[] args) {
 		Scanner tastiera = new Scanner(System.in);
 		double prezzo = 0;
-		String risposta = "+---------------------------------------+\n|\tScheda Tecnica:\t\t\t|\n|\tCPU = ";
+		String cornice = "+---------------------------------------+";
+		String risposta = cornice + "\n|\tScheda Tecnica:\t\t\t|\n|\tCPU = ";
+		int vali3 = 30, vali5 = 80, vali7 = 150;
+		int ddr3 = 1, ddr4 = 3, ddr5 = 5;
+		int valoreGaming = 150;
+		
 		
 		System.out.println("Seleziona modello di CPU: i3, i5, i7");
-		String cpu = tastiera.nextLine();
-				
-		switch (cpu.toLowerCase()) {
-			case "i3":
-				prezzo += 30;
-			break;
-			case "i5":
-				prezzo += 80;
-			break;
-			case "i7":
-				prezzo += 150;
-			break;
-			default:
-				System.out.println("Mi dispiace ma non risulta inserita una CPU valida!");
-			break;
-		}
-		
+		String cpu = tastiera.nextLine().toLowerCase();
 		System.out.println("Seleziona modello di RAM: DDR3, DDR4, DDR5");
-		String ram = tastiera.nextLine();
+		String ram = tastiera.nextLine().toUpperCase();
 		System.out.println("Indica quanti GB di RAM hai bisogno:");
 		int gbRam = Integer.parseInt(tastiera.nextLine());
-		
-		if(gbRam>64)
-			System.out.println("Mi dispiace ma non possediamo abbastanza RAM in ufficio");
+		boolean isGaming = gbRam>=16 && (cpu.equals("i5")||cpu.equals("i7"));
+		boolean schedaValida = true;
 				
-		switch (ram.toUpperCase()) {
+		switch (cpu) {
+			case "i3":
+				prezzo += vali3;
+				break;
+			case "i5":
+				prezzo += vali5;
+				break;
+			case "i7":
+				prezzo += vali7;
+				break;
+			default:
+				schedaValida = false;
+				System.out.println("ATTENZIONE! non risulta inserita una CPU valida!");
+				break;
+		}
+		
+		switch (ram) {
 			case "DDR3":
-				prezzo += 1*gbRam;
+				prezzo += ddr3*gbRam;
 			break;
 			case "DDR4":
-				prezzo += 3*gbRam;
+				prezzo += ddr4*gbRam;
 			break;
 			case "DDR5":
-				prezzo += 5*gbRam;
+				prezzo += ddr5*gbRam;
 			break;
 			default:
-				System.out.println("Mi dispiace ma non risulta inserita una RAM valida!");
+				prezzo = 0;
+				schedaValida = false;
+				System.out.println("ATTENZIONE! non risulta inserita una RAM valida!");
 			break;
 		}
 		
+		if(gbRam>64) {
+			schedaValida = false;
+			System.out.println("Mi dispiace ma non possediamo abbastanza RAM in ufficio");
+		}
 		
+		if(isGaming)
+			prezzo += valoreGaming;
 		
-		if(gbRam>=16 && (cpu.equalsIgnoreCase("i5")||cpu.equalsIgnoreCase("i7")))
-			prezzo += 150;
-		
-		risposta = risposta + cpu +
-					"\t\t\t|\n|\tRAM = " + ram.toUpperCase() + " da " + gbRam + "GB" +
-					"\t\t|\n|\tPrezzo Finale = " + prezzo + " €\t\t|\n+---------------------------------------+";
-		
-		System.out.println(risposta);
+		if(schedaValida) {
+			risposta += cpu +
+						"\t\t\t|\n|\tRAM = " + ram + " da " + gbRam + "GB" +
+						"\t\t|\n|\tPrezzo Finale = " + prezzo + " €\t\t|\n" + cornice;
+			System.out.println(risposta);
+		} else 
+			System.out.println("\nNon sono state inserite specifiche corrette");
 		
 		tastiera.close();
 
