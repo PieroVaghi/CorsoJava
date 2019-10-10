e091735b973f244a0a165d68b349691d14e155db
utente15 <utente15@M-3-015.station>
2019-10-10 14:39:37 +0200

    VERDE - menuITA

--- Scuola0.1/src/test/Programma.java
+++ Scuola0.1/src/test/Programma.java
@@ -1,9 +1,23 @@
 package test;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Programma {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+
+		String menuITA = "";
+		String percorso = "src/res/menuITA.txt";
+		Scanner datiMenu = new Scanner(new File(percorso));
+		while(datiMenu.hasNextLine()) {
+			menuITA += datiMenu.nextLine() + "\n";
+		}
+		datiMenu.close();
+		System.out.println(menuITA);
+		
+		
+		
 
 	}
 
