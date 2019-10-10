99463266e485b13f8567a4ec2f8905ee315d287f
utente15 <utente15@M-3-015.station>
2019-10-10 15:37:57 +0200

    VERDE - Classe studente inizializzata

--- Scuola0.1/src/entities/Studente.java
+++ Scuola0.1/src/entities/Studente.java
@@ -2,4 +2,29 @@ package entities;
 
 public class Studente {
 
+	public String nome;
+	public String cognome;
+	public String dataNascita;
+	public String genere;
+	public double mediaIta;
+	public double mediaIng;
+	public double mediaInf;
+	public double mediaMat;
+	public String media() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String somma() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public boolean promosso() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public String erasmus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
