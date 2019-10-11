07aa01df277ba563160aaad0a8bf3e065b52bff4
utente15 <utente15@M-3-015.station>
2019-10-11 10:39:39 +0200

    Esercizi sui vettori

---
+++ Vettori/src/test/Programma2.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Programma2 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 77;
+		System.out.println(a);
+		a = 85;
+		System.out.println(a);
+		a = 33;
+		System.out.println(a);
+		a = 42;
+		System.out.println(a);
+		a = 77;
+		System.out.println(a);
+		int contatore = 0;
+		
+	}
+
+}
