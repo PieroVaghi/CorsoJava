beda6b2a61aeda34eeeaa8f02c66791c1929a4a4
utente15 <utente15@M-3-015.station>
2019-10-22 12:55:42 +0200

    Validati dati classe Studente

--- Scuola4/src/entities/Studente.java
+++ Scuola4/src/entities/Studente.java
@@ -5,6 +5,8 @@ public class Studente extends Persona {
 	String sezione;
 	public double mediaing, mediainf, mediaita, mediamate;
 	
+	public static final String SEZIONIVALIDE = "1a,1b,1c,2a,2b,2c,3a,3b,3c,4a,4b,4c,5a,5b,5c";
+	
 	public Studente(int id, String nome, String cognome, String datanascita, String genere, String cf, String sezione) {
 		super(id, nome, cognome, datanascita, genere, cf);
 		this.sezione = sezione;
@@ -30,6 +32,36 @@ public class Studente extends Persona {
 		return mediamate;
 	}
 	
+	public static boolean isValido(String[] parti) {
+		boolean validazionePapa = Persona.isValido(parti);
+		return 	validazionePapa 			&& 
+				isSezioneValida(parti[7])	&&
+				isVotoValido(parti[8])		&&
+				isVotoValido(parti[9])		&&
+				isVotoValido(parti[10])		&&
+				isVotoValido(parti[11]);
+	}
+	
+	public static boolean isSezioneValida(String sezione) {
+		return(SEZIONIVALIDE.indexOf(sezione)>0 && sezione.indexOf(",")<0);		
+	}
+	public static boolean isVotoValido (String voto) {
+		double mediaInt = Double.parseDouble(voto);
+		if(mediaInt>0 && mediaInt<11)
+			return true;
+		else
+			return false;
+	}
+	
+	public String toString() {
+		String risposta = super.toString() + "\n";
+		risposta += "Sezione: " 	+ sezione 	+ ",\n" +
+					"Italiano: " 	+ mediaita 	+ ",\n" +
+					"Inglese: " 	+ mediaing 	+ ",\n" +
+					"Informatica: "	+ mediainf 	+ ",\n" +
+					"Matematica: " 	+ mediamate	;
+		return risposta;
+	}
 	
 	
 }
