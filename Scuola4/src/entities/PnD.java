926e87adb1b7c2356d57896e89d5883894262fe6
utente15 <utente15@M-3-015.station>
2019-10-22 14:54:31 +0200

    toString e toCSV implementati in classe PND

--- Scuola4/src/entities/PnD.java
+++ Scuola4/src/entities/PnD.java
@@ -21,9 +21,57 @@ public class PnD extends Impiegato {
 		super(id, nome, cognome, datanascita, genere, cf, tipo, stipendio, anniesp);
 		this.ruolo = ruolo;
 	}
-
+	
 	public String getRuolo() {
 		return ruolo;
 	}
+	
+	public static boolean isValido(String[] parti) {
+		boolean validazionePapa = Impiegato.isValido(parti);
+		return 	validazionePapa 				&& 
+				isRuoloValido(parti[9])			;
+				
+				
+	}
+	
+	private static boolean isRuoloValido(String ruolo) {
+		if(ruolo.isEmpty())
+			return false;
+			switch (ruolo.toLowerCase()) {
+				case "segreteria":
+				case "tecnicolab":
+				case "bidelleria":
+				case "direzione":
+					return true;
+				default: 
+					return false;
+			}
+	}
+	
+	public double stipendio() {
+		switch(ruolo.toLowerCase()) {
+			case "segreteria":
+				return super.getStipendio() + super.getAnniesp()*20;
+			case "tecnicolab":
+				return super.getStipendio() + super.getAnniesp()*50;
+			case "bidelleria":
+				return super.getStipendio() + super.getAnniesp()*10;
+			case "direzione":
+				return super.getStipendio() + super.getAnniesp()*80;
+			default: return -1;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String risposta = super.toString() + "\n";
+		return  risposta + ",\nRuolo: " + ruolo + "Stipendio Effettivo: " + stipendio() +" €";
+	}
+	
+	public String toCSV() {
+		String csvPapa = super.toCSV();
+		return csvPapa+","+ruolo+","+stipendio();
+	}
+
 
 }
