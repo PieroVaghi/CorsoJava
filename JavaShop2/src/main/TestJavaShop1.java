c2b1e7a5c2407d63c1d6be982e4ccf96e2f7c051
utente15 <utente15@192.168.11.70>
2019-10-23 15:17:32 +0200

    Test JavaShop1

---
+++ JavaShop2/src/main/TestJavaShop1.java
@@ -0,0 +1,95 @@
+package main;
+
+import java.io.File;
+import java.util.Scanner;
+
+import aggregatore.Negozio;
+
+public class TestJavaShop1 {
+
+	public static void main(String[] args) throws Exception{
+		
+		Negozio shop = new Negozio();
+		
+		Scanner datimenu = new Scanner(new File("src/res/MenuJavaShop1.txt"));
+		String menu = "";
+		while(datimenu.hasNextLine()) {
+			menu += datimenu.nextLine() + "\n";
+		}
+		datimenu.close();
+		
+		Scanner tastiera = new Scanner(System.in);
+		int comando = 0;
+		
+		do {
+			System.out.println(menu);
+			comando = Integer.parseInt(tastiera.nextLine());
+			String risposta = "";
+			switch (comando) {
+				case 0:
+					risposta = "Ciao Bello!";
+				break;
+				case 1:
+					risposta = "Il numero di prodotti del negozio è: " + shop.nprodotti() + "\n";
+				break;
+				case 2:
+					risposta = "Il numero totale di Pc presenti in negozio è: " + shop.npc() + "\n";
+				break;
+				case 3:
+					risposta = "Il numero totale dei Laptop presenti in negozio è: " + shop.nlaptop() + "\n";
+				break;
+				case 4:
+					System.out.println("Inserisci la CPU da ricercare: ");
+					risposta = shop.nprodottipercpu(tastiera.nextLine()) + "Prodotti del nostro negozio posseggono la CPU indicata\n";
+				break;
+				case 5:
+					risposta = "Il costo totale di tutti i prodotti del mio negozio è: " + shop.totaleprezzi() + "\n";
+				break;
+				case 6:
+					risposta = "Il media di tutti i prodotti del mio negozio è: " + shop.media() + "\n";
+				break;	
+				case 7:
+					risposta = "Il prezzo più basso del negozio è: " + shop.minimo() + "\n";
+				break;
+				case 8:
+					risposta = "Il prezzo più alto del negozio è: " + shop.massimo() + "\n";
+				break;
+				case 9:
+					risposta = "Ecco i computer più economici: " + shop.schedepiueconomici() + "\n";
+				break;
+				case 10:
+					risposta = "Ecco i computer più costosi: " + shop.schedepiucostosi() + "\n";
+				break;
+				case 11:
+					risposta = "Ecco i computer da Gaming: " + shop.schedegaming() + "\n";
+				break;
+				case 12:
+					risposta = "Ecco i computer da ufficio: " + shop.schedeoffice() + "\n";
+				break;
+				case 13:
+					System.out.println("Inserisci l'ID da ricercare: ");
+					int id = Integer.parseInt(tastiera.nextLine());
+					risposta = shop.ricerca(id) + "\n";
+				break;
+				case 14:
+					System.out.println("Inserisci l'ID da ricercare: ");
+					String cpu = tastiera.nextLine();
+					risposta = shop.ricerca(cpu) + "\n";
+				break;
+				case 15:
+					System.out.println("Inserisci un tetto di prezzo: ");
+					double prezzo = Double.parseDouble(tastiera.nextLine());
+					risposta = shop.ricerca(prezzo) + "\n";
+				break;
+				default:
+					risposta = "Hai inserito un comando non valido BALUBA!!!";
+				break;
+			}
+			System.out.println(risposta);
+		} while (comando != 0);
+		
+		tastiera.close();
+
+	}
+
+}
