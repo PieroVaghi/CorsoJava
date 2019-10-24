640f9cf2a61c8eccdb1c61e5d8eaf529f777af1b
utente15 <utente15@M-3-015>
2019-10-24 11:45:22 +0200

    Cambiato nome metodo

--- JavaShop2/src/main/TestIncapsulamento.java
+++ JavaShop2/src/main/TestIncapsulamento.java
@@ -7,9 +7,9 @@ public class TestIncapsulamento {
 	public static void main(String[] args) {
 		Prodotto p = new Prodotto(1,"Candy", "LavatriceFantasma",300);
 		System.out.println(p.getPrezzobase());
-		p.setPrezzo(-1000);
+		p.setPrezzobase(-1000);
 		System.out.println("bnfuilednhuficnlzsehfuevnilaguyerilvnaf: " + p.getPrezzobase());
-		p.setPrezzo(100);
+		p.setPrezzobase(100);
 		System.out.println("boffonchiare: " + p.getPrezzobase());
 	}
 
