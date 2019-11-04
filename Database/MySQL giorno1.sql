b512a6e15f10f42f045dc73e4e62bc54f907eef9
PieroVaghi <piero.vaghi@gmail.com>
2019-11-04 17:34:57 +0100

    Add files via upload

--- Database/MySQL giorno1.sql
+++ Database/MySQL giorno1.sql
@@ -106,4 +106,75 @@ where d.ruolo = 'programmatore';
 -- voglio visualizzare le persone che hanno l'iniziale nome compreso tra la lettera L e la lettera P
 select nome, cognome
 from dipendenti
-where nome between 'l' and 'p';
+where nome between 'l' and 'p'; -- OCCHIO che si ferma a p
+
+-- voglio visualizzare i nominativi per ordine alfabetico decrescente
+select concat(nome, ' ', cognome) nominativo
+from dipendenti
+order by nominativo desc;
+
+-- voglio visualizzare i nominativi per ordine alfabetico decrescente
+select concat(nome, ' ', cognome) nominativo
+from dipendenti
+order by concat(nome, ' ', cognome) desc; -- alcuni casi di DBMS non gestiscono gli alias e quindi dobbiamo scrivere così
+
+-- voglio visualizzare i nomi e cognomi per ordine alfabetico decrescente e i cognomi in ordine alfabetico crescente
+select nome, cognome 
+from dipendenti
+order by nome desc, cognome asc; -- il primo ha prevalenza, in caso di uguaglianze passa alla seconda condizione
+
+-- visualizzare le persone che sono nate tra il 1980 e 1999
+select *
+from dipendenti
+where datanascita between '1980-01-01' and '1999-12-31';
+-- Usiamo la funzione scalare year
+select *, dayname(datanascita) giorno, year(datanascita) anno
+from dipendenti
+where year(datanascita) between 1980 and 1999
+order by anno
+-- Esiste anche month e day
+
+-- visualizza che giorno era nel mese e nella settimana quando sono nati i nostri dipendenti
+select dayofyear(datanascita) giornoAnno, dayofmonth(datanascita) giornoMese, dayofweek(datanascita) giornoSettimana
+from dipendenti
+-- domenica è 1
+
+-- visualizza che giorno era nel mese e nella settimana quando sono nati i nostri dipendenti
+select dayofyear(datanascita) giornoAnno, dayofmonth(datanascita) giornoMese, dayofweek(datanascita) giornoSettimana, dayname(datanascita) nomeGiorno
+from dipendenti
+-- domenica è 1
+
+-- visualizza l'età dei dipenenti
+select year(now()) - year(datanascita) eta
+from dipendenti
+order by eta;
+select floor(datediff(now(), datanascita)/365.25) eta
+from dipendenti
+order by eta;
+-- datediff restituisce differenza in giorni
+
+-- voglio visualizzare il numero dei dipendenti
+select count(*)
+from dipendenti
+
+-- voglio calcolare la somma di tutti gli stipendi
+-- count è l'unica funzione di gruppo che può avere l'asterisco tra le tonde
+-- le altre devono per forza avee almeno una colonna come strumento di calcolo
+select sum(stipendio)
+from dipendenti;
+
+-- voglio calcolare la media di tutti gli stipendi
+select concat(round(avg(stipendio),2),' €') madiaStipendi
+from dipendenti;
+
+-- voglio visualizzare lo stipendio più alto
+-- MODO 1 - Furbo
+select nome, stipendio
+from dipendenti
+order by stipendio desc limit 1; -- OCCHIO!! la seconda persona, nel caso di identico stipendio, non verrà visualizzata
+-- MODO 2 - Saggio e con la conoscenza delle funzioni di gruppo
+select concat(round(max(stipendio)),' €') StipendioMax
+from dipendenti;
+-- MODO 2 - SBAGLIATO!!!!
+select nome, concat(round(max(stipendio)),' €') StipendioMax 
+from dipendenti; -- SBAGLIATO!! ci restituisce il primo nome! non quello dello stipendio più alto! 
