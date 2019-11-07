6cda48a9f12c5f28b46e52b82c1cde08d30069b5
utente15 <utente15@M-3-015>
2019-11-07 17:47:08 +0100

    Classe aggregatrice con metodo di lettura query

---
+++ Azienda/src/aggregatore/Azienda.java
@@ -0,0 +1,41 @@
+package aggregatore;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import db.Database;
+import enities.*;
+
+public class Azienda {
+	
+	private List<Dipartimento> dipartimenti = new ArrayList<Dipartimento>();
+	private Database db;
+	
+	public Azienda(String percorso, String username, String pw) {
+		db = new Database(percorso, username, pw);
+		caricaDipartimenti();
+	}
+
+	public void caricaDipartimenti() {
+		db.apriConnessione();
+		String query = "SELECT * FROM dipartimenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet tabsql = s.executeQuery(query);							// un oggetto ResultSet  è un'intera tabella di SQL, sia dati che metadati
+			while(tabsql.next()) {
+				Dipartimento d = new Dipartimento(	tabsql.getInt("id"),
+													tabsql.getInt("massimacapienza"),
+													tabsql.getString("nome"),
+													tabsql.getString("citta"),
+													tabsql.getDouble("budget"));
+				dipartimenti.add(d);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		db.chiudiConnessione();
+	}
+	
+}
