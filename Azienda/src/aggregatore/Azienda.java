3734cf25f2e94e713770bd2c9d05103ef937cee1
utente15 <utente15@M-3-015>
2019-11-08 17:02:27 +0100

    Implementata classe aggregatrice

--- Azienda/src/aggregatore/Azienda.java
+++ Azienda/src/aggregatore/Azienda.java
@@ -8,7 +8,7 @@ import java.util.List;
 import db.Database;
 import enities.*;
 
-public class Azienda {
+public class Azienda implements IAzienda{
 	
 	private List<Dipartimento> dipartimenti = new ArrayList<Dipartimento>();
 	private Database db;
@@ -22,8 +22,8 @@ public class Azienda {
 		db.apriConnessione();
 		String query = "SELECT * FROM dipartimenti;";
 		try {
-			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
-			ResultSet tabsql = s.executeQuery(query);							// un oggetto ResultSet  è un'intera tabella di SQL, sia dati che metadati
+			Statement s = db.getConnection().createStatement(); 				// createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet tabsql = s.executeQuery(query);							// un oggetto ResultSet è un'intera tabella di SQL, sia dati che metadati
 			while(tabsql.next()) {
 				Dipartimento d = new Dipartimento(	tabsql.getInt("id"),
 													tabsql.getInt("massimacapienza"),
@@ -57,7 +57,7 @@ public class Azienda {
 												rs.getInt("iddipartimento"),
 												rs.getString("dataassunzione"),
 												rs.getInt("idresponsabile"),
-												rs.getDouble("budget"));
+												rs.getDouble("stipendio"));
 				dipendenti.add(d);
 				for (Dipartimento dip : dipartimenti) {
 					if(dip.aggiungiDipendente(d))
@@ -69,6 +69,135 @@ public class Azienda {
 		}
 		db.chiudiConnessione();
 		return dipendenti;
+	}
+	
+	public List<Lingua> caricaLingue() {
+		List<Lingua> lingue = new ArrayList<Lingua>();
+		db.apriConnessione();
+		String query = "select lingue.id, lingue.nome, lingue.bonus, parla.iddipendente, parla.competenza from parla join lingue on parla.idlingua = lingue.id";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);							// un oggetto ResultSet  è un'intera tabella di SQL, sia dati che metadati
+			while(rs.next()) {
+				Lingua l = new Lingua(	rs.getInt("id"),
+												rs.getInt("competenza"),
+												rs.getInt("iddipendente"),
+												rs.getString("nome"),
+												rs.getDouble("bonus"));
+				lingue.add(l);
+				for (Dipartimento dip : dipartimenti) 
+					for(Dipendente d : dip.getDipendenti())
+						if(d.aggiungiLingua(l))
+							break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		db.chiudiConnessione();
+		return lingue;
+	}
+
+	public int numDip() {
+		int cont = 0;
+		for(Dipartimento dipa : dipartimenti)
+			cont += dipa.getDipendenti().size();
+		return cont;
+	}
+	
+	@Override
+	public double totstipendi() {
+		double ris = 0;
+		for(Dipartimento dipa : dipartimenti)
+			for(Dipendente dipe : dipa.getDipendenti())
+				ris += dipe.getStipendio();
+	return ris;
+	}
+
+	@Override
+	public double avgstipendi() {
+		return totstipendi()/numDip();
+	}
+
+	@Override
+	public double minstipendi() {
+		double min = Double.MAX_VALUE;
+		for(Dipartimento dipa : dipartimenti)
+			for(Dipendente dipe : dipa.getDipendenti())
+				min = (min > dipe.getStipendio()) ? dipe.getStipendio() : min;
+	return min;
+	}
+
+	@Override
+	public double maxstipendi() {
+		double max = Double.MIN_VALUE;
+		for(Dipartimento dipa : dipartimenti)
+			for(Dipendente dipe : dipa.getDipendenti())
+				max = (max < dipe.getStipendio()) ? dipe.getStipendio() : max;
+	return max;
+	}
+
+	@Override
+	public double totstipendiNonORM() {
+		db.apriConnessione();
+		String query = "select round(sum(stipendio),2) totStipendi from dipendenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);
+			return (rs.next() && rs != null) ? rs.getDouble("totStipendi") : 0;		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			db.chiudiConnessione();
+		}
+	}
+
+	@Override
+	public double avgstipendiNonORM() {
+		db.apriConnessione();
+		String query = "select round(avg(stipendio),2) totStipendi from dipendenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);
+			return (rs.next() && rs != null) ? rs.getDouble("totStipendi") : 0;		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			db.chiudiConnessione();
+		}
+	}
+
+	@Override
+	public double minstipendiNonORM() {
+		db.apriConnessione();
+		String query = "select round(min(stipendio),2) totStipendi from dipendenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);
+			return (rs.next() && rs != null) ? rs.getDouble("totStipendi") : 0;		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			db.chiudiConnessione();
+		}
+	}
+
+	@Override
+	public double maxstipendiNonORM() {
+		db.apriConnessione();
+		String query = "select round(max(stipendio),2) totStipendi from dipendenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);
+			return (rs.next() && rs != null) ? rs.getDouble("totStipendi") : 0;		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			db.chiudiConnessione();
+		}
 	}
 	
 }
