c576be74b7263c8f20b0abe5b2959d65502b58b6
utente15 <utente15@M-3-015>
2019-11-08 11:12:56 +0100

    Metodo caricaDipendenti

--- Azienda/src/aggregatore/Azienda.java
+++ Azienda/src/aggregatore/Azienda.java
@@ -38,4 +38,37 @@ public class Azienda {
 		db.chiudiConnessione();
 	}
 	
+	public List<Dipendente> caricaDipendenti() {
+		List<Dipendente> dipendenti = new ArrayList<Dipendente>();
+		db.apriConnessione();
+		String query = "SELECT * FROM azienda.dipendenti;";
+		try {
+			Statement s = db.getConnection().createStatement(); 				//createStatement() è lo strumento che ci consente di eseguire le query
+			ResultSet rs = s.executeQuery(query);							// un oggetto ResultSet  è un'intera tabella di SQL, sia dati che metadati
+			while(rs.next()) {
+				Dipendente d = new Dipendente(	rs.getInt("id"),
+												rs.getInt("mensilita"),
+												rs.getString("nome"),
+												rs.getString("cognome"),
+												rs.getString("genere"),
+												rs.getString("dataNascita"),
+												rs.getString("ruolo"),
+												rs.getString("citta"),
+												rs.getInt("iddipartimento"),
+												rs.getString("dataassunzione"),
+												rs.getInt("idresponsabile"),
+												rs.getDouble("budget"));
+				dipendenti.add(d);
+				for (Dipartimento dip : dipartimenti) {
+					if(dip.aggiungiDipendente(d))
+						break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		db.chiudiConnessione();
+		return dipendenti;
+	}
+	
 }
