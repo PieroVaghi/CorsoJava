4e12ae18e1f93c8043990e71a3be43a804882086
utente15 <utente15@M-3-015>
2019-11-12 14:10:02 +0100

    Committo roba..

---
+++ Shop/src/entities/Person.java
@@ -0,0 +1,35 @@
+package entities;
+
+public class Person {
+	
+	private int id;
+	private String name, surname, dob;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	
+	
+
+}
