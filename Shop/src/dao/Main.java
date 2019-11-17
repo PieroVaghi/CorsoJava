f16b441c64c462b7cfc506aae189741f56bf6ea6
PieroVaghi <pierovaghi@gmail.com>
2019-11-17 15:18:33 +0100

    VERDE - Main Person

--- Shop/src/dao/Main.java
+++ Shop/src/dao/Main.java
@@ -3,6 +3,8 @@ import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import entities.Client;
+import entities.Employee;
 import entities.Person;
 
 
@@ -22,33 +24,46 @@ public class Main
          //come valore la query di salvataggio
          Map<String, String> saveQueries = new HashMap<String,String>();
          
-         saveQueries.put("Employee", "insert into Person values([id],'[name]','[surname]','[dob]');insert into Employee values([id],[salary],'[mansion]');");
+         saveQueries.put("Employee", "insert into Person values([id],'[name]','[surname]','[dob]');insert into Employee values([id],'[mansion]',[salary]);");
          saveQueries.put("Client", "insert into Person values([id],'[name]','[surname]','[dob]');insert into Client values([id],'[email]','[interests]');");
          EntityDAO<Person> dao = new FlatEntityDAO<Person>
          (
         	conn,
          	"select * from viewperson",
          	"select * from viewperson where id=",
-         	"delete from person where id=[id];delete from employee where id=[id];delete from client where id=[id];",
+         	"delete from person where id=[id]",//;delete from employee where id=[id];delete from client where id=[id]",
          	saveQueries
         );
      
          
          
-         System.out.println(dao.list());
+        System.out.println(dao.list());
          
-         /*
+         
          Employee e = new Employee();
          
-         e.setName("Viktor");
+         e.setName("Walter");
          e.setSurname("Frankenstein");
          e.setDob("01/01/1979");
-         e.setMansion("scientist");
-         e.setSalary(10000);
+         e.setMansion("Venditore");
+         e.setSalary(1500);
          e.setId(2);
          
          dao.save(e);
-         */
+         
+         Client c = new Client();
+         
+         c.setName("Francesco");
+         c.setSurname("Rotondi");
+         c.setDob("27/12/1992");
+         c.setEmail("frarotonfi@gmail.com");
+         c.setInterests("Vespa, Pompieti, Margherita");
+         c.setId(4);
+         
+         dao.save(c);
+         
+         System.out.println(dao.list());
+         
 	}
 
 }
