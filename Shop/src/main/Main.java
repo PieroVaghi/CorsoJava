7c95017752f852f8a670c10299e090a985e4c2e4
utente15 <utente15@M-3-015>
2019-11-18 09:18:55 +0100

    ritorno in classe

--- Shop/src/main/Main.java
+++ Shop/src/main/Main.java
@@ -10,7 +10,7 @@ import entities.*;
 
 public class Main 
 {
-	private static final String PWD = "piefragio1";
+	private static final String PWD = "root";
 	
 	public static void main(String[] args) throws Exception 
 	{
