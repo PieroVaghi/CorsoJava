a4286e4923291f8de4a0b46f5284b6e6200fc7e7
utente15 <utente15@M-3-015>
2019-11-21 16:46:24 +0100

    Modificato FlatEntityDAO in grazie a Database

--- Generation/src/generation/common/dao/FlatEntityDAO.java
+++ Generation/src/generation/common/dao/FlatEntityDAO.java
@@ -5,22 +5,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import generation.common.database.Database;
 import generation.common.entities.*;
 
 public class FlatEntityDAO<E extends generation.common.entities.Entity> implements EntityDAO<E>
 {
 	//DIPENDENZE
-	Connection connection;
+	Database database;
 	String dataquery;
 	String datasinglequery;
 	String deletequery;
 	Map<String,String> saveQueries = new HashMap<String,String>();
 	Factory factory;
 	
-	public FlatEntityDAO(Connection connection, String dataquery, String datasinglequery, String deletequery, Map<String,String> saveQueries, Factory factory)
+	public FlatEntityDAO(Database database, String dataquery, String datasinglequery, String deletequery, Map<String,String> saveQueries, Factory factory)
 	{
 		//la query da cui prendere i dati
-		this.connection = connection;
+		this.database = database;
 		this.dataquery = dataquery;
 		this.datasinglequery = datasinglequery;
 		this.deletequery = deletequery;
@@ -34,32 +35,24 @@ public class FlatEntityDAO<E extends generation.common.entities.Entity> implemen
 	{
 		E res = null;
 		String sql = datasinglequery+id;
-		Statement command = connection.createStatement();
-		ResultSet row = command.executeQuery(sql);
-
-		if(row.next())
-			res = (E) factory.make(_rowToMap(row));
-		
-		row.close();
-		command.close();
-		
-		return res;
+		database.row(sql);
+		return res = (E) factory.make(database.row(sql));
 	}
 
 
-	private Map<String, String> _rowToMap(ResultSet row) throws Exception
-	{
-		Map<String,String> res = new HashMap<String,String>();
-		
-		for(int i=1;i<=row.getMetaData().getColumnCount();i++)
-		{
-			String fieldName = row.getMetaData().getColumnLabel(i);	//nome del campo
-			String fieldValue = row.getString(i);					//valore
-			res.put(fieldName, fieldValue);
-		}
-		
-		return res;		
-	}
+//	private Map<String, String> _rowToMap(ResultSet row) throws Exception
+//	{
+//		Map<String,String> res = new HashMap<String,String>();
+//		
+//		for(int i=1;i<=row.getMetaData().getColumnCount();i++)
+//		{
+//			String fieldName = row.getMetaData().getColumnLabel(i);	//nome del campo
+//			String fieldValue = row.getString(i);					//valore
+//			res.put(fieldName, fieldValue);
+//		}
+//		
+//		return res;		
+//	}
 
 
 	@Override
@@ -72,16 +65,10 @@ public class FlatEntityDAO<E extends generation.common.entities.Entity> implemen
 	@Override
 	public boolean delete(int id) throws Exception
 	{
-		Statement command = connection.createStatement();
-		
 		String sql = deletequery.replace("[id]",(id+""));
-//		System.out.println(sql);
-		command.execute(sql);
-		
-		command.close();
-		
+		database.executeOnDb(sql);
 		return true;
-		
+	
 	}
 
 
@@ -98,10 +85,8 @@ public class FlatEntityDAO<E extends generation.common.entities.Entity> implemen
 		
 		for(String cmd:sql.split(";"))
 		{
-			Statement command = connection.createStatement();
-//			System.out.println(cmd);
-			command.execute(cmd);
-			command.close();
+			database.executeOnDb(cmd);
+			return true;
 		}
 		return true;
 	}
@@ -116,14 +101,9 @@ public class FlatEntityDAO<E extends generation.common.entities.Entity> implemen
 	public List<E> list(String condition) throws Exception 
 	{
 		List<E> res = new ArrayList<E>();
-		Statement command = connection.createStatement();
-//		System.out.println(dataquery + " where "+ condition);
-		ResultSet rows = command.executeQuery(dataquery + " where "+ condition);
-		while(rows.next())
-			res.add((E) factory.make(_rowToMap(rows)));		
-			
-		rows.close();
-		command.close();
+		String sql = dataquery + " where "+ condition;
+		for(Map<String,String> m : database.rows(sql))
+			res.add((E) factory.make(m));		
 		return res;	
 	}
 	
